3121f70eb68e5293a73c2c22d1b72439365197fe
Maruthi Sayampu <maruthi.sayampu@gmail.com>
2021-06-06 00:08:17 +0800

    Updated DTO obejcts

--- payment-service/src/main/java/com/mybank/data/entities/InboundPayments.java
+++ payment-service/src/main/java/com/mybank/data/entities/InboundPayments.java
@@ -8,9 +8,9 @@ import javax.persistence.Table;
 import com.sun.istack.NotNull;
 
 @Entity
-@Table(name="inbound_payments")
+@Table(name = "inbound_payments")
 public class InboundPayments {
-	
+
 	@Id
 	private String referenceId;
 	@Column
@@ -18,72 +18,88 @@ public class InboundPayments {
 	@Column
 	private String creditAccountId;
 	@NotNull
-    private int amount;
+	private int amount;
 	@Column
-    private String sourceCurrency;
+	private String sourceCurrency;
 	@Column
-    private String destinationCurrency;
+	private String destinationCurrency;
 	@Column
-    private String transferCurrency;
+	private String transferCurrency;
 	@Column
-    private String valueDate;
+	private String valueDate;
 	@Column
-    private String partyId;
+	private String partyId;
+
 	public String getReferenceId() {
 		return referenceId;
 	}
+
 	public void setReferenceId(String referenceId) {
 		this.referenceId = referenceId;
 	}
+
 	public String getDebitAccountId() {
 		return debitAccountId;
 	}
+
 	public void setDebitAccountId(String debitAccountId) {
 		this.debitAccountId = debitAccountId;
 	}
+
 	public String getCreditAccountId() {
 		return creditAccountId;
 	}
+
 	public void setCreditAccountId(String creditAccountId) {
 		this.creditAccountId = creditAccountId;
 	}
+
 	public int getAmount() {
 		return amount;
 	}
+
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+
 	public String getSourceCurrency() {
 		return sourceCurrency;
 	}
+
 	public void setSourceCurrency(String sourceCurrency) {
 		this.sourceCurrency = sourceCurrency;
 	}
+
 	public String getDestinationCurrency() {
 		return destinationCurrency;
 	}
+
 	public void setDestinationCurrency(String destinationCurrency) {
 		this.destinationCurrency = destinationCurrency;
 	}
+
 	public String getTransferCurrency() {
 		return transferCurrency;
 	}
+
 	public void setTransferCurrency(String transferCurrency) {
 		this.transferCurrency = transferCurrency;
 	}
+
 	public String getValueDate() {
 		return valueDate;
 	}
+
 	public void setValueDate(String valueDate) {
 		this.valueDate = valueDate;
 	}
+
 	public String getPartyId() {
 		return partyId;
 	}
+
 	public void setPartyId(String partyId) {
 		this.partyId = partyId;
 	}
-    
-	
-    
+
 }
